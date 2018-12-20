e18bd731de845f791227a84457b55c668e3d9f62
Dmitriy-Blahodatnyi <dbrlmola82@gmail.com>
2018-12-20 19:22:36 +0200

    Starting blazen project

---
+++ blazen/src/main/java/com/cloud/blazen/BlazenApplication.java
@@ -0,0 +1,22 @@
+package com.cloud.blazen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class BlazenApplication {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(BlazenApplication.class, args);
+    }
+
+}
+
