b88bf7cae08e7f5ca10d6ecbc77420bcd79d8778
Dmitriy-Blahodatnyi <dbrlmola82@gmail.com>
2019-02-25 14:08:45 +0200

    exception

--- server/src/main/java/controller/CustomerController.java
+++ server/src/main/java/controller/CustomerController.java
@@ -1,10 +1,16 @@
 package controller;
 
+import model.customer.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 import service.customer.CustomerService;
+
+import javax.validation.Valid;
+import java.util.Collection;
 
 @RestController
 @RequestMapping("api/v1/customers")
@@ -17,5 +23,12 @@ public class CustomerController {
     }
 
     @GetMapping
+    public Collection<Customer> getAllCustomers() {
+        return service.getAll();
+    }
 
+    @PostMapping
+    public Customer createCustomer(@Valid @RequestBody Customer customer) {
+        return service.create(customer);
+    }
 }
--- server/src/main/java/repository/dao/CustomerDAO.java
+++ server/src/main/java/repository/dao/CustomerDAO.java
@@ -1,11 +1,15 @@
 package repository.dao;
 
+import exception.ResourceNotFoundException;
 import model.customer.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import repository.CustomerRepository;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 @Component
 public class CustomerDAO {
@@ -24,11 +28,19 @@ public class CustomerDAO {
         repository.deleteById(id);
     }
 
+    public Customer update(Long id, List<Consumer> list) {
+        var customer = getById(id);
+        
+    }
+
     public Collection<Customer> getAll() {
         return repository.findAll();
     }
 
     public Customer getById(Long id) {
-        return repository.getOne(id);
+        var customer = repository.findById(id);
+        return customer.orElseThrow(() ->
+                new ResourceNotFoundException("Customer with id: " + id + " is not found!!!")
+        );
     }
 }
